5c9706440d358b68df96e759e78428fcaa59f096
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2024-12-16 20:20:26 +0200

    feat(DAL): added UnitOfWork for repos

---
+++ Data/Data/UnitOfWork.cs
@@ -0,0 +1,37 @@
+using Data.Interfaces;
+using Data.Repositories;
+
+namespace Data.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly SocialNetworkDbContext _context;
+    
+    public UnitOfWork(SocialNetworkDbContext context)
+    {
+        _context = context;
+        ConversationParticipantRepository = new ConversationParticipantRepository(_context);
+        ConversationRepository = new ConversationRepository(_context);
+        FriendshipRepository = new FriendshipRepository(_context);
+        GroupDetailsRepository = new GroupDetailsRepository(_context);
+        MessageRepository = new MessageRepository(_context);
+        UserRepository = new UserRepository(_context);
+    }
+    
+    public IConversationParticipantRepository ConversationParticipantRepository { get; }
+    
+    public IConversationRepository ConversationRepository { get; }
+    
+    public IFriendshipRepository FriendshipRepository { get; }
+    
+    public IGroupDetailsRepository GroupDetailsRepository { get; }
+    
+    public IMessageRepository MessageRepository { get; }
+    
+    public IUserRepository UserRepository { get; }
+    
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
